5209daf14142246e0bf0d8bef479412cb5d23b8f
cchuuu <chuyuanz@nycstudents.net>
2024-10-29 12:28:50 -0400

    a

---
+++ vite-project/JS/main.js
@@ -0,0 +1,124 @@
+import "../CSS/style.css";
+
+const Electronics = [
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+  {
+    name: "name", //name of the item
+    price: "price", //price of the item
+    type: "type", //cameras, computers, mouses, keyboards
+    image: "", //url of image for item
+  },
+];
